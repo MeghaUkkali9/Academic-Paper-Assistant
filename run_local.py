fb268edf853e67253c4816b8a26171304d7736a4
MeghaUkkali9 <meghaukkali99@gmail.com>
2026-07-06 04:14:57 +0530

    testing

--- run_local.py
+++ run_local.py
@@ -6,9 +6,13 @@ logging.basicConfig(level=logging.INFO)
 
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
-from airflow.dags.arxiv_tasks.index_papers import index_research_papers 
+from src.arxiv_tasks.index_papers import index_research_papers 
+from src.arxiv_tasks.ingest_papers import ingest_papers 
 
 if __name__ == "__main__":
+    result1 = ingest_papers()
+    print("\n--- RESULT ---")
+    print(result1)
     result = index_research_papers() 
     print("\n--- RESULT ---")
     print(result)
